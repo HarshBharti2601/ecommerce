ecbd42efb3df78319e197cbf5c496fbbf981d093
HarshBharti2601 <harshbharti2601@gmail.com>
2024-01-31 18:39:15 +0530

    initial commit

---
+++ backend/Controllers/productController.js
@@ -0,0 +1,44 @@
+const Product = require("../models/productModel");
+
+
+
+
+exports.createproduct=async(req,res,next)=>{
+
+
+    const product=await Product.create(req.body);
+
+
+    res.status(201).json({
+        success:true,
+        product
+    });
+};
+
+exports.getAllProducts=async(req,res)=>{
+
+  const products=await Product.find();
+
+    res.status(200).json({
+        success:true,
+        products
+    });
+
+ exports.updateProduct=async(req,res,next)=>{
+    let product=await Product.findById(req.params.id);
+    if(!product){
+       return res.status(500).json({
+        success:false,
+        message:"product not found"
+       })  
+    }
+    product=await product.findByIdandUpdate(req.params.id,req.body);
+
+
+
+    re.status(200).json({
+        success:true,
+        product
+    })
+ }  
+};
---
+++ backend/models/productModel.js
@@ -0,0 +1,63 @@
+const mongoose=require("mongoose");
+
+
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please add the product name"],
+        trim:true
+    },
+    description:{
+        type:String,
+        required:[true,"Please add the product description"]
+     },
+     price:{
+        type:Number,
+        required:[true,"Please add the price of the product"],
+        maxLength:[8,"price is out of range"]
+     },
+     rating:{
+        type:Number,
+        default:0
+     },
+     images:[{
+        public_id:{
+            type:String,
+            required:true
+        },
+        url:{
+            type:String,
+            required:true
+        }
+     }],
+     stock:{
+        type:Number,
+        required:true,
+        maxLength:[4,"too much"],
+        default:1
+     } ,
+     numofReviews:{
+        type:Number,
+        default:0
+     } ,
+     reviews:[{
+       name:{
+        type:String,
+        required:true
+       },
+       rating:{
+        type:Number,
+        required:true
+       },
+       comment:{
+        type:String,
+        required:true
+       }
+     } 
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+});
+module.exports=mongoose.model("Product",productSchema);
